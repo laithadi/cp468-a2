a8824455982b0f2bf7c0976f2ce40240df2c8859
Laith Adi <laith_adi@hotmail.com>
2020-11-05 23:24:38 -0500

    Update cell.py

--- cell.py
+++ cell.py
@@ -1,5 +1,6 @@
-class cell:
+class Cell:
 
-    def __init__(self, domain, value = None):
+    def __init__(self, value = None, index = None):
         self.domain = (1,2,3,4,5,6,7,8,9)
-        self.value = value          
+        self.value = value    
+        self.index = index       
