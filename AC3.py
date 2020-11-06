0b939bb0d4c4bf1d7926aee701cbb6e0d1c1afc4
Ethan Coulter <coltsyy@gmail.com>
2020-11-06 13:36:02 -0500

    Create AC3.py

---
+++ AC3.py
@@ -0,0 +1,10 @@
+def AC3(csp):
+    
+    """ 
+    Uses the CSP or arc consistency to check each individual constraint.
+    Returns False if an inconsistency is found within the constraints, 
+    otherwise True
+    
+    """
+
+    
