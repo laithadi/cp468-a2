b4608b07e21363d06dd776952da39ff83e59ac20
Laith Adi <laith_adi@hotmail.com>
2020-11-05 23:24:37 -0500

    Update constraints.py

--- constraints.py
+++ constraints.py
@@ -1,28 +1,82 @@
-# generate a new cell() for each cell in the sudoku 
+from readingInput import readInputTxtFile
+from temp import createCell  
+
+SUDOKU = readInputTxtFile() 
+
+CELLS = createCell(SUDOKU)
+
+def con_rows(cells):
+
+    for cell in cells:
+        
+        row = cells[cell].index[0]
+        col = cells[cell].index[1]
+
+        for i in range(0,9):
+            if i != col:
+                if cells[cell].value == cells['cell'+str(row+1)+str(i+1)].value:
+                    return False
+
+    return True 
 
 
+def con_cols(cells):
+
+    for cell in cells:
+
+        row = cells[cell].index[0]
+        col = cells[cell].index[1]
+
+        for i in range(0,9):
+            if i != row:
+                if cells[cell].value == cells['cell'+str(i+1)+str(col+1)]:
+                    return False
+    
+    return True
 
 
+def con_box(cells):
+
+    for cell in cells:
+
+        row = cells[cell].index[0]
+        col = cells[cell].index[1]
+
+        norm_row = row % 3 
+        norm_col = col % 3
+
+        if norm_row == 0:
+            for newrow in range(1,3):
+
+                if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+1)]: # checks value straight below cell 
+                    return False 
+
+                if norm_col == 0: 
+                    for newcol in range(1,3):
+
+                        if cells[cell].value == cells['cell'+str(row+1)+str(col+newcol+1)]: # checks value straight to the right 
+                            return False
+
+                        if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+newcol+1)]:
+                            return False 
+
+                if norm_col == 1:
+
+                    if cells[cell].value == cells['cell'+str(row+1)+str(col+1+1)]: # checks value straight to the right 
+                            return False
+
+                    if cells[cell].value == cells['cell'+str(row+1)+str(col+1-1)]: # checks value straight to the right 
+                            return False
+
+                    if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+1+1)]: # checks value straight to the right 
+                            return False
+
+                    if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+1-1)]: # checks value straight to the right 
+                            return False
 
 
+                if norm_col == 2:
 
-
-
-# from cell.py import cell 
-
-# cell(x,y) = new cell() 
-
-
-# def con_row(cell):
-
-#     cell (i,j) 
-
-#     for l in range(0,9):
-#         if sod[i][l]  == sod[][]:
-#             return False 
-
-
-# def con_col
-
-# def con_box
-
+                    pass
+    
+    return True 
