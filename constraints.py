c032a141b8b8294b80f51504c67fad9e9d9de2d6
Laith Adi <laith_adi@hotmail.com>
2020-11-05 20:57:29 -0500

    Update constraints.py

--- constraints.py
+++ constraints.py
@@ -1,3 +1,28 @@
-from cell.py import cell  
+# generate a new cell() for each cell in the sudoku 
 
+
+
+
+
+
+
+
+
+# from cell.py import cell 
+
+# cell(x,y) = new cell() 
+
+
+# def con_row(cell):
+
+#     cell (i,j) 
+
+#     for l in range(0,9):
+#         if sod[i][l]  == sod[][]:
+#             return False 
+
+
+# def con_col
+
+# def con_box
 
