2dd774fbda5a359f9de47c9c24310a2749fc7c70
Laith Adi <laith_adi@hotmail.com>
2020-11-05 23:24:27 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,44 +1,68 @@
-def getInputInd():
-    """
-    returns a tuple of indices (i,j) for cells with a given value from input.txt 
-    """
-    pass
+from readingInput import readInputTxtFile
+from cell import Cell
 
-def createCell():
+SUDOKU = readInputTxtFile()
+
+def createCell(sudoku):
     """
-    Create the constaint for each vacant cell. FORM: ()
-        - go through each vacant cell and create new cell() 
+    Create the constaint for each cell. 
     """
 
-def getRows(index):
-    """
-    returns a tuple with the indices (i,j) in the same row as the argument -- index.
-    """
-    pass 
+    all_cells = {}
 
-def getCol(index):
-    """
-    returns a tuple with the indices (i,j) in the same column as the argument -- index.
-    """
-    pass 
+    for r in range(0,9):
+        for c in range(0,9):
+            all_cells['cell'+str(r+1)+str(c+1)] = Cell(value=sudoku[r][c], index=(r,c))
 
-def getBox(index):
-    """
-    returns a tuple with the indices (i,j) in the same box as the argument -- index.
-    """
-    pass 
+    return all_cells
 
-def updatesDomain(index):
-    #get the valye of index
-    # call the getrows 
-    # getcol
-    # getbox 
-    #for loop that goes to all the above indices and calls cell.con[] == 0
-    pass    
 
-def check_con_one():
-    """
-    checks for all constraints and sees which constraints has one 1 in it. we call cell.vailable_vals
-    """
-    pass
+
+
+
+
+
+
+
+
+
+
+
+# def getInputInd():
+#     """
+#     returns a tuple of indices (i,j) for cells with a given value from input.txt 
+#     """
+#     pass
+
+# def getRows(index):
+#     """
+#     returns a tuple with the indices (i,j) in the same row as the argument -- index.
+#     """
+#     pass 
+
+# def getCol(index):
+#     """
+#     returns a tuple with the indices (i,j) in the same column as the argument -- index.
+#     """
+#     pass 
+
+# def getBox(index):
+#     """
+#     returns a tuple with the indices (i,j) in the same box as the argument -- index.
+#     """
+#     pass 
+
+# def updatesDomain(index):
+#     #get the valye of index
+#     # call the getrows 
+#     # getcol
+#     # getbox 
+#     #for loop that goes to all the above indices and calls cell.con[] == 0
+#     pass    
+
+# def check_con_one():
+#     """
+#     checks for all constraints and sees which constraints has one 1 in it. we call cell.vailable_vals
+#     """
+#     pass
     
