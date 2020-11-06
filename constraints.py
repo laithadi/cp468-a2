1b1cf7a5500f592b9569e38f52a321f90f079995
Laith Adi <laith_adi@hotmail.com>
2020-11-05 23:29:47 -0500

    Update constraints.py

--- constraints.py
+++ constraints.py
@@ -35,48 +35,52 @@ def con_cols(cells):
     return True
 
 
-def con_box(cells):
-
-    for cell in cells:
-
-        row = cells[cell].index[0]
-        col = cells[cell].index[1]
-
-        norm_row = row % 3 
-        norm_col = col % 3
-
-        if norm_row == 0:
-            for newrow in range(1,3):
-
-                if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+1)]: # checks value straight below cell 
-                    return False 
-
-                if norm_col == 0: 
-                    for newcol in range(1,3):
-
-                        if cells[cell].value == cells['cell'+str(row+1)+str(col+newcol+1)]: # checks value straight to the right 
-                            return False
-
-                        if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+newcol+1)]:
-                            return False 
-
-                if norm_col == 1:
-
-                    if cells[cell].value == cells['cell'+str(row+1)+str(col+1+1)]: # checks value straight to the right 
-                            return False
-
-                    if cells[cell].value == cells['cell'+str(row+1)+str(col+1-1)]: # checks value straight to the right 
-                            return False
-
-                    if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+1+1)]: # checks value straight to the right 
-                            return False
-
-                    if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+1-1)]: # checks value straight to the right 
-                            return False
 
 
-                if norm_col == 2:
 
-                    pass
+
+# def con_box(cells):
+
+#     for cell in cells:
+
+#         row = cells[cell].index[0]
+#         col = cells[cell].index[1]
+
+#         norm_row = row % 3 
+#         norm_col = col % 3
+
+#         if norm_row == 0:
+#             for newrow in range(1,3):
+
+#                 if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+1)]: # checks value straight below cell 
+#                     return False 
+
+#                 if norm_col == 0: 
+#                     for newcol in range(1,3):
+
+#                         if cells[cell].value == cells['cell'+str(row+1)+str(col+newcol+1)]: # checks value straight to the right 
+#                             return False
+
+#                         if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+newcol+1)]:
+#                             return False 
+
+#                 if norm_col == 1:
+
+#                     if cells[cell].value == cells['cell'+str(row+1)+str(col+1+1)]: # checks value straight to the right 
+#                             return False
+
+#                     if cells[cell].value == cells['cell'+str(row+1)+str(col+1-1)]: # checks value straight to the right 
+#                             return False
+
+#                     if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+1+1)]: # checks value straight to the right 
+#                             return False
+
+#                     if cells[cell].value == cells['cell'+str(row+newrow+1)+str(col+1-1)]: # checks value straight to the right 
+#                             return False
+
+
+#                 if norm_col == 2:
+
+#                     pass
     
-    return True 
+#     return True 
