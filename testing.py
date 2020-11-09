da1606f3e65a92de4ba120372456f83338b61807
Laith Adi <laith_adi@hotmail.com>
2020-11-09 17:14:29 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,38 +1,46 @@
 from AC3 import AC3
 from backtrack import backtrackCSP
 from constraints import CSP
-from temp import SUDOKU
+from readingInput import readInputTxtFile
 
-def solve(grid, index, total):
+SUDOKU = readInputTxtFile()
+
+def solve(sudoku):
     
-    print("\nSudoku {}/{} : \n{}".format(index, total, print_grid(grid)))
+    # print("\nSudoku {}/{} : \n".format(index, total))
 
 
-    print("{}/{} : AC3 starting".format(index, total))
+    # print("{}/{} : AC3 starting".format(index, total))
 
 
     # instanciate Sudoku
-    cons = Sudoku(grid)
+    print(sudoku)
+    cons = CSP(sudoku)
+    print(cons)
 
     # launch AC-3 algorithm of it
     AC3_result = AC3(cons)
 
     # Sudoku has no solution
     if not AC3_result:
-        print("{}/{} : this sudoku has no solution".format(index, total))
+        # print("{}/{} : this sudoku has no solution".format(index, total))
+        print("errorrrrrrrrrr")
 
     else:
         
         # check if AC-3 algorithm has solve the Sudoku
-        if cons.isFinished():
+        if cons.completed():
 
-            print("{}/{} : AC3 was enough to solve this sudoku !".format(index,total))
-            print("{}/{} : Result: \n{}".format(index, total, cons))
+            # print("{}/{} : AC3 was enough to solve this sudoku !".format(index,total))
+
+            # print("{}/{} : Result: \n{}".format(index, total, cons))
+            print("COMPLETED")
 
         # continue the resolution
         else:
 
-            print("{}/{} : AC3 finished, Backtracking starting...".format(index,total))
+            # print("{}/{} : AC3 finished, Backtracking starting...".format(index,total))
+            
 
             assignment = {}
 
@@ -50,7 +58,12 @@ def solve(grid, index, total):
                 cons.possibilities[cell] = assignment[cell] if len(cell) > 1 else cons.possibilities[cell]
             
             if assignment:
-                print("{}/{} : Result: \n{}".format(index, total, cons))
+                # print("{}/{} : Result: \n{}".format(index, total, cons))
+                print("wbalabalf")
 
             else:
-                print("{}/{} : No solution exists".format(index, total))
+                # print("{}/{} : No solution exists".format(index, total))
+                print("hithere")
+
+
+solve(SUDOKU)
