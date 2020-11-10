9d5a4c484582c4a3feedc22455e216171cae72b4
Laith Adi <laith_adi@hotmail.com>
2020-11-09 20:05:53 -0500

    Update backtrack.py

--- backtrack.py
+++ backtrack.py
@@ -1,11 +1,8 @@
 from readingInput import readInputTxtFile
-from temp import createCell  
-from cell import Cell
 from constraints import CSP
 from helper import constraint_check, assign, unassign
 
 def backtrackCSP(answer, CSP):
-    
     """
     Instantly checks if the length of our answer is equivalent to the
     amount of cells in SUDOKU.
@@ -13,8 +10,6 @@ def backtrackCSP(answer, CSP):
     If there are constraint issues, it will unassign cells,
     reassign possibilities to the neighbouring cells,
     and loop again.
-    
-    
     If our length of answer quals the length of CSP cells, we have solved the puzzle.
     """
 
@@ -28,13 +23,10 @@ def backtrackCSP(answer, CSP):
         if constraint_check(CSP, answer, cell, val):
             assign(CSP, cell, val, answer)
             result = backtrackCSP(answer, CSP)
-
-
             if result:
                 return result
             
             unassign(CSP, cell, answer)
-
 
     return False
 
